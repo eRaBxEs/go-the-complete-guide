a900dc1ba49f0ffcd87f053652ec0e4f0d1174b2
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-12-21 15:31:42 +0000

    alternative way of variable declaration

---
+++ code/02-go-essentials/03-using-alternative-variable-declaration-styles/investment_calculator.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var investmentAmount float64 = 1000
+	expectedReturnRate := 5.5
+	var years = 10.0
+	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate)/100, years)
+	fmt.Println(futureValue)
+}
