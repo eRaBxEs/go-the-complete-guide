53fc78c4eff085cdac02e37a488169adbadecb4c
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-13 05:51:54 +0000

    exercise with if

---
+++ code/02-go-essentials/16-exercise-if-statements/bank.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var accountBalance float64 = 1000.0
+
+	fmt.Println("Welcome to Go bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Enter your choice: ")
+	fmt.Scan(&choice)
+	fmt.Println("Your Choice: ", choice)
+
+	if choice == 1 {
+		fmt.Println("Your account balance is", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Balance updated! New update:", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Your Withdrawal Amount:")
+		var wihdrawalAmount float64
+		accountBalance -= wihdrawalAmount
+		fmt.Println("Balance updated! New update:", accountBalance)
+	}
+}
