9a43633ac3fc8e540bff569520a7b2baf6382eaf
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-16 05:00:04 +0000

    creating a struct

---
+++ code/05-struct-custom-types/02-defining-a-struct/structs.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// ... do something awesome with that gathered data!
+
+	outputUserDetails(firstName, lastName, birthdate)
+}
+
+func outputUserDetails(firstName, lastName, birthdate string) {
+	fmt.Println(firstName, lastName, birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
