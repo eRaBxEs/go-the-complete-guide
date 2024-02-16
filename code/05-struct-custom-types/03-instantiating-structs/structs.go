7cdb5bf538adc10da3bb0641a416af2768819b17
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-16 05:18:48 +0000

    instantiating a struct

---
+++ code/05-struct-custom-types/03-instantiating-structs/structs.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user // declare a varaiable of type custom struct before initialising below
+
+	appUser = user{ // composite literal or struct literal
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	outputUserDetails(appUser)
+}
+
+func outputUserDetails(userDetails user) {
+	fmt.Println(userDetails)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
