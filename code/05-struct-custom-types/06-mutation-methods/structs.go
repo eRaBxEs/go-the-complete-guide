095a01e1d24022f9ecc339c13848c111a69bbecd
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-16 06:55:22 +0000

    mutating the member of a struct thru the receiver

---
+++ code/05-struct-custom-types/06-mutation-methods/structs.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func (u user) outputUserDetails() { // here we have (u user) just before the name of the function and this is called receiver
+	fmt.Println(u.firstName, u.lastName, u.birthdate) // go allows u to access the fields without the need to dereference
+}
+
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	appUser := user{ // composite literal or struct literal
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
+
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
