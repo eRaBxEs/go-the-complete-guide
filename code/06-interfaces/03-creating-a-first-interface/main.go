16d0982b6bc848a31245393dcf505246ef9984ee
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-20 07:58:48 +0000

    adding an interface in the main

--- code/06-interfaces/03-creating-a-first-interface/main.go
+++ code/06-interfaces/03-creating-a-first-interface/main.go
@@ -6,27 +6,27 @@ import (
 	"os"
 	"strings"
 
-	"example.com/note/note"
-	"example.com/note/todo"
+	"example.com/interface/02-finishing-interface-preparations/note"
+	"example.com/interface/02-finishing-interface-preparations/todo"
 )
 
-type saver interface {
+type saver interface { // using a convention in go to name an interface based on the method contract it posseses
 	Save() error
 }
 
 func main() {
+
 	title, content := getNoteData()
-	todoText := getUserInput("Todo text: ")
+
+	todoText := getUserInput("Todo text:")
 
 	todo, err := todo.New(todoText)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	userNote, err := note.New(title, content)
-
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -34,9 +34,8 @@ func main() {
 
 	todo.Display()
 	err = todo.Save()
-
 	if err != nil {
-		fmt.Println("Saving the todo failed.")
+		fmt.Println("Saving the todo failed!")
 		return
 	}
 
@@ -44,21 +43,18 @@ func main() {
 
 	userNote.Display()
 	err = userNote.Save()
-
 	if err != nil {
-		fmt.Println("Saving the note failed.")
+		fmt.Println("Saving the note failed!")
 		return
 	}
 
 	fmt.Println("Saving the note succeeded!")
-}
-
-func saveData(data note.Note) {
 
 }
 
 func getNoteData() (string, string) {
 	title := getUserInput("Note title:")
+
 	content := getUserInput("Note content:")
 
 	return title, content
@@ -67,16 +63,17 @@ func getNoteData() (string, string) {
 func getUserInput(prompt string) string {
 	fmt.Printf("%v ", prompt)
 
-	reader := bufio.NewReader(os.Stdin)
-
-	text, err := reader.ReadString('\n')
+	// rather use fmt.Scanln use buffer Reader
+	reader := bufio.NewReader(os.Stdin)  // os.Stdin indicates that we are reading from the command line
+	text, err := reader.ReadString('\n') // delimiter : to indicate where we want to stop reading
 
 	if err != nil {
 		return ""
 	}
 
+	// Now after getting the read data in text, we will need to remove the line break from it (\n)
 	text = strings.TrimSuffix(text, "\n")
-	text = strings.TrimSuffix(text, "\r")
+	text = strings.TrimSuffix(text, "\r") // removing \r because on windows each line break comes with a carriage return "\r"
 
 	return text
 }
